e1d5d43a406dd3d0c4330b380d5485d1b464c739
Denzel <denzel_silva@Torrico.com>
2022-12-09 20:34:18 -0500

    todo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,17 +10,22 @@ export class AppComponent {
   name = ""
   lastname = ""
   afecta = false
-  
+  id = 1
   datos = [{
+      id:1,
       name:"denzel",
       lastname:"Torrico"
   }]
 
   agregar(name:string,lastname:string) {
-    this.datos.push({name:name,lastname:lastname})
+    this.id+=1
+    this.datos.push({id:this.id,name:name,lastname:lastname})
     this.name = ""
     this.lastname = ""
-  
+  }
+  Eliminar(id:number){
+    this.datos = this.datos.filter(function(d){return d.id !==id})
+    //console.log(this.datos.filter(function(d){return d.id !==id}))
   }
   
  
